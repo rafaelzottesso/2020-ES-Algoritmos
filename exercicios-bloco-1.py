4b3b097e9d971a4d83b33d555b452ea98fd449ce
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-06-09 12:45:32 -0300

    Resolução Ex. 1 Bloco 1.

---
+++ exercicios-bloco-1.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome:")
+cidade = input("Digite sua cidade:")
+pais = input("Digite o nome dos seus pais:")
+idade = int(input("Digite sua idade:"))
+
+
+print("Seu nome é:", nome)
+print("Você mora em", cidade)
+print("Você é filho de", pais)
+
+print("Você tem", idade, "anos.")
