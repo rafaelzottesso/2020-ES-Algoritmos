4777a24cdbed983e158eca8d798cc0d6faa8ba75
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-06-09 14:13:19 -0300

    Exercício 5 e Desafio 3.

--- exercicios-bloco-1.py
+++ exercicios-bloco-1.py
@@ -1,11 +1,35 @@
+"""
+Exercício 5
 nome = input("Digite seu nome:")
 cidade = input("Digite sua cidade:")
 pais = input("Digite o nome dos seus pais:")
 idade = int(input("Digite sua idade:"))
 
-
 print("Seu nome é:", nome)
 print("Você mora em", cidade)
 print("Você é filho de", pais)
-
 print("Você tem", idade, "anos.")
+
+
+# Exercício 5
+x = input("Digite um número: ")
+x = int(x)
+y = int(input("Outro número: "))
+
+z = x + y
+
+print("A soma de X + Y é", z)
+"""
+
+# Desafio 3
+horas = int( input("Horas: ") )
+minutos = int( input("Minutos: ") )
+segundos = int( input("Segundos: ") )
+
+tempo = horas * 3600 + minutos * 60 + segundos
+
+# horas = horas * 3600
+# minutos = minutos * 60
+# tempo = horas + minutos + segundos
+
+print("O tempo total do atleta foi", tempo, "segundos.")
