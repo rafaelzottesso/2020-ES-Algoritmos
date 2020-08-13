f9ae34c84cce3b12f2772854f31647f6a8257a83
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-08-12 21:01:39 -0300

    Introdução ao laço de repetição while.

---
+++ 2020-08-12 - Revisão e repetição com while.py
@@ -0,0 +1,92 @@
+
+# print('Bem vindos - 12/08/2020')
+
+# # nome = "Rafael Zottesso" #string - str
+# # idade = 32 # inteiro - int
+# # salario = 5000.99 # decimal - float
+
+# nome = input("Qual seu nome?")
+# idade = int(input("Quantos anos você tem? "))
+# salario = float(input("Quanto você recebe por mês? "))
+
+# print("Seu nome é", nome, "você tem", idade, "anos e seu salário é", salario)
+
+# nasc = 2020 - idade
+
+# print("Você nasceu em", nasc)
+
+# # if(idade > 18):
+# #     print("Entrada permitida!")
+# #     # código a mais
+    
+# # else:
+# #     print("Acesso negado!")
+# #     # pode ter mais código
+
+
+# print("Introdução ao while")
+
+
+# if(idade >= 18):
+#     print("IF - Entrada permitida! ", idade)
+    
+#     idade = idade - 1
+    
+
+# while(idade >= 18):
+#     print("While - Entrada permitida! ", idade)
+    
+#     idade = idade - 2
+
+
+# print("O comando while acabou!")
+
+# print("Início do programa que diminui")
+# x = 10
+
+# while(x >= 1):
+#     print("O valor de x é", x)
+#     x = x - 1
+# print("Fim do programa!")
+
+# print("Iniciando programa que aumenta...")
+# x = 1
+# while(x <= 100):
+#     print("X equivale a", x)
+#     x = x + 1
+# print("Fim do programa")
+
+
+# print("Adivinhe o número da sorte...")
+# i = 0
+
+# while(i != 7):
+#     i = int(input("Acerte o número: "))
+    
+# print("Você acertou!")
+
+
+print("Adivinhe o número da sorte...")
+i = 0
+numero = 7
+
+while(i != numero):
+    
+    i = int(input("Qual o número da sorte? "))
+    
+    if(i == numero):
+        print("Você acertou, parabéns!")
+    else:
+        print("Você errou, tente novamente...")
+        
+
+
+
+
+
+
+
+
+
+
+
